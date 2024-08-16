4ebee921e9f22b69c8e295b5141297741fbfb66e
Miss JAV <byu.ambon1@gmail.com>
2024-08-17 00:51:37 +0900

    Create player.js

---
+++ player.js
@@ -0,0 +1,8 @@
+
+
+// ngalihin ke web player
+
+        function astroag31() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://canhabsyi.github.io/";
+        }
