82f79b60cb260150973f86a322a948d1b06fbeac
Miss JAV <byu.ambon1@gmail.com>
2024-08-17 01:25:31 +0900

    Update player.js

--- player.js
+++ player.js
@@ -1,23 +1,14 @@
+// Video pertama yang akan ditampilkan saat halaman dimuat
+const videoPertama = 'https://canhabsyi.github.io/plyr/?url=https://ssc1-ak.akamaized.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd&keyId=d84c325f36814f39bbe59080272b10c3&key=550727de4c96ef1ecff874905493580f';
 
+// Atur video pertama saat halaman dimuat
+document.addEventListener('DOMContentLoaded', function() {
+    gantiVideo(videoPertama);
+});
 
 // ngalihin ke web player
 
         function tes1() {
             var frame = document.getElementById("vidio");
             frame.src = "https://ambontv.my.id/plyr/jw.html?url=https://op-group1-swiftservehd-1.dens.tv/h/h10/index.m3u8?app_type=web&userid=lite&chname=RTV";
-        }
-
-        function tes2() {
-            var frame = document.getElementById("vidio");
-            frame.src = "https://canhabsyi.github.io/";
-        }
-
-        function tes3() {
-            var frame = document.getElementById("vidio");
-            frame.src = "https://canhabsyi.github.io/";
-        }
-
-        function tes4() {
-            var frame = document.getElementById("vidio");
-            frame.src = "https://canhabsyi.github.io/";
         }
