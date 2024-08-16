eea44370915b14b1a4b13aba67415f479de7fbdc
Miss JAV <byu.ambon1@gmail.com>
2024-08-17 00:55:14 +0900

    Update player.js

--- player.js
+++ player.js
@@ -2,7 +2,22 @@
 
 // ngalihin ke web player
 
-        function astroag31() {
+        function tes1() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://canhabsyi.github.io/plyr?url=https://op-group1-swiftservehd-1.dens.tv/h/h10/index.m3u8?app_type=web&userid=lite&chname=RTV";
+        }
+
+        function tes2() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://canhabsyi.github.io/";
+        }
+
+        function tes3() {
+            var frame = document.getElementById("vidio");
+            frame.src = "https://canhabsyi.github.io/";
+        }
+
+        function tes4() {
             var frame = document.getElementById("vidio");
             frame.src = "https://canhabsyi.github.io/";
         }
